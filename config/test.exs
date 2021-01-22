04b355d8ec01f3dd76c8e47af93f8a2f360c0c1a
Matt Overstreet <matt.overstreet@gmail.com>
2021-01-22 15:50:43 +0000

    removed some test config that has moved

--- config/test.exs
+++ config/test.exs
@@ -1,9 +1,3 @@
 use Mix.Config
 
 config :logger, level: :debug
-
-config :astra,
-  id: System.get_env("ASTRA_ID"),
-  region: System.get_env("ASTRA_REGION"),
-  username: System.get_env("ASTRA_USERNAME"),
-  password: System.get_env("ASTRA_PASSWORD")
