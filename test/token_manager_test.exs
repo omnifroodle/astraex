e4195b28ab5e64c72e8acad3dbbd956fc21efe22
Matt Overstreet <matt.overstreet@gmail.com>
2021-03-04 22:10:17 +0000

    add a test to token manager for static application tokens

--- test/token_manager_test.exs
+++ test/token_manager_test.exs
@@ -1,7 +1,12 @@
 defmodule Loom.Astra.TokenManagerTest do
   use ExUnit.Case, async: true
 
-  test "get token" do
+  test "get remote token" do
     {:ok, _} = Astra.TokenManager.get_token()
+  end
+  
+  test "get application token from local config" do
+    {:ok, token} = Astra.TokenManager.get_current_token(nil, nil, "12345")
+    assert token == "12345"
   end
 end
